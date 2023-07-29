bf99c55bf03edef79ca08c76605a3b98a4ab7993
Laura Velasco <maldolt@nv.ccsd.net>
2023-07-28 19:53:43 -0700

    fixed current user

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,24 +1,40 @@
-const router = require('express').Router()
-const db = require("../models")
-const bcrypt = require('bcrypt')
+const router = require("express").Router();
+const db = require("../models");
+const bcrypt = require("bcrypt");
 
-const { User } = db
+const { User } = db;
 
-router.post('/', async (req, res) => {
-    
+router.post("/", async (req, res) => {
+  let user = await User.findOne({
+    where: { email: req.body.email },
+  });
+
+  if (
+    !user ||
+    !(await bcrypt.compare(req.body.password, user.passwordDigest))
+  ) {
+    res
+      .status(404)
+      .json({
+        message: `Could not find a user with the provided username and password`,
+      });
+  } else {
+    req.session.userId = user.userId;
+    res.json({ user });
+  }
+});
+
+router.get("/profile", async (req, res) => {
+  try {
     let user = await User.findOne({
-        where: { email: req.body.email }
-    })
+      where: {
+        userId: req.session.userId,
+      },
+    });
+    res.json(user);
+  } catch {
+    res.json(null);
+  }
+});
 
-    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
-        res.status(404).json({ 
-            message: `Could not find a user with the provided username and password` 
-        })
-    } else {
-        res.json({ user })
-    }
-})
-
-
-
-module.exports = router
+module.exports = router;
