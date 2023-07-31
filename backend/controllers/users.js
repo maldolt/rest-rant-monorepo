71eba559d35633df5c49dad5b497cbb04b0d88f9
Laura Velasco <maldolt@nv.ccsd.net>
2023-07-30 18:02:00 -0700

    day 5

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -5,10 +5,11 @@ const bcrypt = require("bcrypt");
 const { User } = db;
 
 router.post("/", async (req, res) => {
-    let {password, ...rest } = req.body;
+  let { password, ...rest } = req.body;
   const user = await User.create({
     ...rest,
-    passwordDigest: await bcrypt.hash(password, 10)
+    role: "reviewer",
+    passwordDigest: await bcrypt.hash(password, 10),
   });
   res.json(user);
 });
