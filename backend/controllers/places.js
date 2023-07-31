7ae9597d93279c99cca111b5c1998232e6b65b8f
Laura Velasco <maldolt@nv.ccsd.net>
2023-07-30 17:37:31 -0700

    middleware

--- backend/controllers/places.js
+++ backend/controllers/places.js
@@ -1,7 +1,7 @@
-const router = require('express').Router()
-const db = require("../models")
+const router = require("express").Router();
+const db = require("../models");
 
-const { Place, Comment, User } = db
+const { Place, Comment, User } = db;
 
 router.post('/', async (req, res) => {
     if (!req.body.pic) {
@@ -17,12 +17,10 @@ router.post('/', async (req, res) => {
     res.json(place)
 })
 
-
-router.get('/', async (req, res) => {
-    const places = await Place.findAll()
-    res.json(places)
-})
-
+router.get("/", async (req, res) => {
+  const places = await Place.findAll();
+  res.json(places);
+});
 
 router.get('/:placeId', async (req, res) => {
     let placeId = Number(req.params.placeId)
@@ -62,24 +60,26 @@ router.put('/:placeId', async (req, res) => {
     }
 })
 
-router.delete('/:placeId', async (req, res) => {
-    let placeId = Number(req.params.placeId)
-    if (isNaN(placeId)) {
-        res.status(404).json({ message: `Invalid id "${placeId}"` })
+router.delete("/:placeId", async (req, res) => {
+  let placeId = Number(req.params.placeId);
+  if (isNaN(placeId)) {
+    res.status(404).json({ message: `Invalid id "${placeId}"` });
+  } else {
+    const place = await Place.findOne({
+      where: {
+        placeId: placeId,
+      },
+    });
+    if (!place) {
+      res
+        .status(404)
+        .json({ message: `Could not find place with id "${placeId}"` });
     } else {
-        const place = await Place.findOne({
-            where: {
-                placeId: placeId
-            }
-        })
-        if (!place) {
-            res.status(404).json({ message: `Could not find place with id "${placeId}"` })
-        } else {
-            await place.destroy()
-            res.json(place)
-        }
+      await place.destroy();
+      res.json(place);
     }
-})
+  }
+});
 
 router.post('/:placeId/comments', async (req, res) => {
     const placeId = Number(req.params.placeId)
@@ -91,48 +91,46 @@ router.post('/:placeId/comments', async (req, res) => {
     })
 
     if (!place) {
-        res.status(404).json({ message: `Could not find place with id "${placeId}"` })
+        return res.status(404).json({ message: `Could not find place with id "${placeId}"` })
     }
 
-    const author = await User.findOne({
-        where: { userId: req.body.authorId }
-    })
-
-    if (!author) {
-        res.status(404).json({ message: `Could not find author with id "${req.body.authorId}"` })
+    if (!req.currentUser) {
+        return res.status(404).json({ message: `You must be logged in to leave a rand or rave.` })
     }
 
     const comment = await Comment.create({
         ...req.body,
+        authorId: req.currentUser.userId,
         placeId: placeId
     })
 
     res.send({
         ...comment.toJSON(),
-        author
+        author: req.currentUser
     })
 })
 
-router.delete('/:placeId/comments/:commentId', async (req, res) => {
-    let placeId = Number(req.params.placeId)
-    let commentId = Number(req.params.commentId)
+router.delete("/:placeId/comments/:commentId", async (req, res) => {
+  let placeId = Number(req.params.placeId);
+  let commentId = Number(req.params.commentId);
 
-    if (isNaN(placeId)) {
-        res.status(404).json({ message: `Invalid id "${placeId}"` })
-    } else if (isNaN(commentId)) {
-        res.status(404).json({ message: `Invalid id "${commentId}"` })
+  if (isNaN(placeId)) {
+    res.status(404).json({ message: `Invalid id "${placeId}"` });
+  } else if (isNaN(commentId)) {
+    res.status(404).json({ message: `Invalid id "${commentId}"` });
+  } else {
+    const comment = await Comment.findOne({
+      where: { commentId: commentId, placeId: placeId },
+    });
+    if (!comment) {
+      res.status(404).json({
+        message: `Could not find comment with id "${commentId}" for place with id "${placeId}"`,
+      });
     } else {
-        const comment = await Comment.findOne({
-            where: { commentId: commentId, placeId: placeId }
-        })
-        if (!comment) {
-            res.status(404).json({ message: `Could not find comment with id "${commentId}" for place with id "${placeId}"` })
-        } else {
-            await comment.destroy()
-            res.json(comment)
-        }
+      await comment.destroy();
+      res.json(comment);
     }
-})
+  }
+});
 
-
-module.exports = router
+module.exports = router;
--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,26 @@
 // Modules and Globals
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
+const defineCurrentUser = require("./middleware/defineCurrentUser");
 
 // Express Settings
-app.use(cors())
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(defineCurrentUser);
 
 // Controllers & Routes
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/places', require('./controllers/places'))
-app.use('/users', require('./controllers/users'))
+app.use("/places", require("./controllers/places"));
+app.use("/users", require("./controllers/users"));
 
 // Listen for Connections
 app.listen(process.env.PORT, () => {
-    console.log(`Listening on ${process.env.PORT}`)
-})
+  console.log(`Listening on ${process.env.PORT}`);
+});
